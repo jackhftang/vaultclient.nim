59b0a0713daf529239001f193d98d934fe91fafa
Jack Tang <me@jackhftang.com>
2020-06-07 03:29:02 +0800

    add loginAppRole

--- src/vaultclient/approle.nim
+++ src/vaultclient/approle.nim
@@ -1,10 +1,29 @@
-import base, asyncdispatch
+import common, base, asyncdispatch
 
-proc loginAppRole*(client: VaultClient, roleId, secretId: string): Future[void] {.async.} =
-  ## Login with approle auth engine. Auto set token. 
-  let res = await client.write("auth/approle/login", %*{
+## see https://www.vaultproject.io/api-docs/auth/approle#list-roles
+## see https://www.vaultproject.io/api-docs/auth/approle#create-update-approle
+## see https://www.vaultproject.io/api-docs/auth/approle#read-approle
+## see https://www.vaultproject.io/api-docs/auth/approle#delete-approle
+## see https://www.vaultproject.io/api-docs/auth/approle#read-approle-role-id
+## see https://www.vaultproject.io/api-docs/auth/approle#update-approle-role-id
+## see https://www.vaultproject.io/api-docs/auth/approle#generate-new-secret-id
+## see https://www.vaultproject.io/api-docs/auth/approle#list-secret-id-accessors
+## see https://www.vaultproject.io/api-docs/auth/approle#read-approle-secret-id
+## see https://www.vaultproject.io/api-docs/auth/approle#destroy-approle-secret-id
+## see https://www.vaultproject.io/api-docs/auth/approle#read-approle-secret-id-accessor
+## see https://www.vaultproject.io/api-docs/auth/approle#destroy-approle-secret-id-accessor
+## see https://www.vaultproject.io/api-docs/auth/approle#create-custom-approle-secret-id
+
+proc loginAppRole*(client: VaultClient, roleId, secretId: string, useToken = true): Future[JsonNode] {.async.} =
+  ## see https://www.vaultproject.io/api-docs/auth/approle#login-with-approle
+  result = await client.write("auth/approle/login", %*{
     "role_id": roleId,
     "secret_id": secretId
   })
-  let token = res["auth"]["client_token"].getStr()
-  client.token = token
+  if useToken:
+    let token = result["auth"]["client_token"].getStr()
+    client.token = token
+  
+
+## see https://www.vaultproject.io/api-docs/auth/approle#read-update-or-delete-approle-properties
+## see https://www.vaultproject.io/api-docs/auth/approle#tidy-tokens
---
+++ src/vaultclient/approle_test.nim
@@ -0,0 +1,12 @@
+# import testutils
+# import common
+# import base
+
+# suite "approle":
+#   var vault: VaultProcess
+
+#   setup:
+#     vault = newVaultProcess()
+        
+#   teardown:
+#     vault.stop()
